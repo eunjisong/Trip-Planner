9845a148dc9c033116430c9077bb4ff3d3fc8b51
manja <anjiemerchant@gmail.com>
2018-01-30 13:00:02 -0500

    pre lunch commit

--- public/bundle.js
+++ public/bundle.js
@@ -60,36 +60,11 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-const markerMaker = __webpack_require__(2)
-console.log("Hello from JavaScript");
-
-const mapboxgl = __webpack_require__(1);
-
-mapboxgl.accessToken = "pk.eyJ1IjoiYW5qaWVtZXJjaGFudCIsImEiOiJjamQxdHdyanoxa2RmMnpvNGc3NjAzZHV2In0.WxWbLVeEZjWn_76BY9YxzQ";
-
-const map = new mapboxgl.Map({
-  container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
-});
-
-
-const marker = markerMaker.markerMaker("activity", [-74.009151, 40.705086]);
-marker.addTo(map);
-
-
-
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var require;var require;(function(f){if(true){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.mapboxgl = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return require(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
@@ -660,10 +635,35 @@ module.exports={"$version":8,"$root":{"version":{"required":true,"type":"enum","
 //# sourceMappingURL=mapbox-gl.js.map
 
 /***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const markerMaker = __webpack_require__(2)
+console.log("Hello from JavaScript");
+
+const mapboxgl = __webpack_require__(0);
+
+mapboxgl.accessToken = "pk.eyJ1IjoiYW5qaWVtZXJjaGFudCIsImEiOiJjamQxdHdyanoxa2RmMnpvNGc3NjAzZHV2In0.WxWbLVeEZjWn_76BY9YxzQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
+const marker = markerMaker.markerMaker("activity", [-74.009151, 40.705086]);
+marker.addTo(map);
+
+
+
+
+/***/ }),
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const mapboxgl = __webpack_require__(1);
+const mapboxgl = __webpack_require__(0);
 
 const markerMaker = (type, coordinates) => {
   const markerDomEl = document.createElement('div');
